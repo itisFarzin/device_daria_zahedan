b53c3f181886fc29caa946ec61e62ad841e3b016
bengris32 <bengris32@protonmail.ch>
2022-08-17 18:53:04 +0100

    zahedan: Patchelf C2 service to link against libavservices_minijail
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I4cddc98e91724c86e7761fee41d4f9f5554c7203
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,7 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
             ;&
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
+            "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
             "${PATCHELF}" --add-needed "libstagefright_foundation.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.lights-service.zahedan)
